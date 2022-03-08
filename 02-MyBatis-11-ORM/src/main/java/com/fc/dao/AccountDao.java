5f0857f91f25d3c855744798bb65e557bb14e3bd
yyqx <123456@qq.com>
2022-03-08 12:47:23 +0800

    第11次MyBatis中ROM

---
+++ 02-MyBatis-11-ORM/src/main/java/com/fc/dao/AccountDao.java
@@ -0,0 +1,17 @@
+package com.fc.dao;
+
+import com.fc.entity.Account;
+import com.fc.entity.TAcount;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface AccountDao {
+    List<Account> findAll();
+
+    List<Account> findAllOnMap();
+
+    List<TAcount> findById(@Param("id") Integer tId);
+
+
+}
---
+++ 02-MyBatis-11-ORM/src/main/java/com/fc/test/ORMTest.java
@@ -0,0 +1,139 @@
+package com.fc.test;
+
+import com.fc.dao.AccountDao;
+import com.fc.entity.Account;
+import com.fc.entity.TAcount;
+import com.fc.util.MyBatisUtil;
+import org.junit.Test;
+
+import java.util.List;
+
+public class ORMTest {
+
+
+
+    @Test
+    public void test() {
+        AccountDao accountDao = MyBatisUtil.getMapper(AccountDao.class);
+
+        List<Account> accounts = accountDao.findAll();
+
+        for (Account account : accounts) {
+            System.out.println(account);
+        }
+
+        MyBatisUtil.commit();
+    }
+
+    @Test
+    public void testMap() {
+        AccountDao accountDao = MyBatisUtil.getMapper(AccountDao.class);
+
+        List<Account> accounts = accountDao.findAllOnMap();
+
+        for (Account account : accounts) {
+            System.out.println(account);
+        }
+
+        MyBatisUtil.commit();
+    }
+
+    @Test
+    public void testUnderscore() {
+        AccountDao accountDao = MyBatisUtil.getMapper(AccountDao.class);
+
+        List<TAcount> acounts = accountDao.findById(2);
+
+        System.out.println(acounts);
+
+        MyBatisUtil.commit();
+    }
+
+
+
+
+    /*
+    *
+    @Test
+    public void test() {
+        AccountDao accountDao = MyBatisUtil.getMapper(AccountDao.class);
+
+        List<Account> accounts = accountDao.findAll();
+
+        for (Account account : accounts) {
+            System.out.println(account);
+        }
+
+        MyBatisUtil.commit();
+    }
+
+    @Test
+    public void testMap() {
+        AccountDao accountDao = MyBatisUtil.getMapper(AccountDao.class);
+
+        List<Account> accounts = accountDao.findAllOnMap();
+
+        for (Account account : accounts) {
+            System.out.println(account);
+        }
+
+        MyBatisUtil.commit();
+    }
+
+    @Test
+    public void testUnderscore() {
+        AccountDao accountDao = MyBatisUtil.getMapper(AccountDao.class);
+
+        List<TAcount> acounts = accountDao.findById(2);
+
+        System.out.println(acounts);
+
+        MyBatisUtil.commit();
+    }
+    *
+    *
+    *
+    *
+    *
+    *
+    *
+    *
+    @Test
+    public void test() {
+        AccountDao accountDao = MyBatisUtil.getMapper(AccountDao.class);
+
+        List<Account> accounts = accountDao.findAll();
+
+        for (Account account : accounts) {
+            System.out.println(account);
+        }
+
+        MyBatisUtil.commit();
+    }
+
+    @Test
+    public void testMap() {
+        AccountDao accountDao = MyBatisUtil.getMapper(AccountDao.class);
+
+        List<Account> accounts = accountDao.findAllOnMap();
+
+        for (Account account : accounts) {
+            System.out.println(account);
+        }
+
+        MyBatisUtil.commit();
+    }
+
+    @Test
+    public void testUnderscore() {
+        AccountDao accountDao = MyBatisUtil.getMapper(AccountDao.class);
+
+        List<TAcount> acounts = accountDao.findById(2);
+
+        System.out.println(acounts);
+
+        MyBatisUtil.commit();
+    }
+    *
+    * */
+}
