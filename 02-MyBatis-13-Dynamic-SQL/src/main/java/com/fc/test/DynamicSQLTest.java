30bbc4da6714ed37c920cdb4add1547a915adea7
yyqx <123456@qq.com>
2022-03-09 18:09:01 +0800

    第13次MyBatis的动态SQL

---
+++ 02-MyBatis-13-Dynamic-SQL/src/main/java/com/fc/test/DynamicSQLTest.java
@@ -0,0 +1,119 @@
+package com.fc.test;
+
+import com.fc.dao.StudentDao;
+import com.fc.entity.Student;
+import com.fc.util.MyBatisUtil;
+import org.junit.Test;
+
+import java.util.List;
+
+public class DynamicSQLTest {
+    @Test
+    public void testFindAll() {
+        StudentDao studentDao = MyBatisUtil.getMapper(StudentDao.class);
+
+        List<Student> students = studentDao.findAll();
+
+        for (Student student : students) {
+            System.out.println(student);
+        }
+
+        MyBatisUtil.commit();
+    }
+
+    @Test
+    public void testFindAllByKeyword() {
+        StudentDao studentDao = MyBatisUtil.getMapper(StudentDao.class);
+
+        List<Student> students = studentDao.findByKeyword("%娜娜%", "娜娜");
+
+        for (Student student : students) {
+            System.out.println(student);
+        }
+
+        MyBatisUtil.commit();
+    }
+
+    @Test
+    public void testFindByStudent() {
+        StudentDao studentDao = MyBatisUtil.getMapper(StudentDao.class);
+
+        Student student = new Student();
+
+        student.setId(3);
+        student.setName("贾晓东");
+        student.setGender("女");
+
+        List<Student> students = studentDao.findByStudent(student);
+
+        for (Student student1 : students) {
+            System.out.println(student1);
+        }
+
+        MyBatisUtil.commit();
+
+
+
+
+
+    }
+
+
+    @Test
+    public void testFindByStudentOnTrim() {
+        StudentDao studentDao = MyBatisUtil.getMapper(StudentDao.class);
+        Student student = new Student();
+        student.setId(3);
+        student.setName("易烊千玺");
+        student.setGender("女");
+
+        List<Student> students = studentDao.findByStudentOnTrim(student);
+
+        for (Student temp : students) {
+            System.out.println(temp);
+        }
+
+        MyBatisUtil.commit();
+    }
+
+    @Test
+    public void testUpdate() {
+        StudentDao studentDao = MyBatisUtil.getMapper(StudentDao.class);
+
+        Student student = new Student();
+        student.setId(6);
+        student.setInfo("123");
+
+        int affectedRows = studentDao.update(student);
+
+        System.out.println("受影响的行数：" + affectedRows);
+
+        MyBatisUtil.commit();
+    }
+
+    @Test
+    public void testUpdateOnTrim() {
+        StudentDao studentDao = MyBatisUtil.getMapper(StudentDao.class);
+
+        Student student = new Student();
+        student.setId(6);
+        student.setInfo("1234");
+
+        int affectedRows = studentDao.updateOnTrim(student);
+
+        System.out.println("受影响的行数：" + affectedRows);
+
+        MyBatisUtil.commit();
+    }
+
+    @Test
+    public void testDeleteMore() {
+        StudentDao studentDao = MyBatisUtil.getMapper(StudentDao.class);
+
+        int affectedRows = studentDao.deleteMore(5, 6, 7, 8);
+
+        System.out.println(affectedRows);
+
+        MyBatisUtil.commit();
+    }
+}
